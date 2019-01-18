ba308886aae30e4faedcad324d28c961063e044e
freelyfeng <974556697@qq.com>
2019-01-18 17:17:43 +0800

    提交文件

---
+++ utils/function.py
@@ -0,0 +1,16 @@
+import time
+import random
+
+
+def get_order_sn():
+    # 获取订单号，保证唯一
+
+    s = '1234567890yeiyrquyerpieiruoiuetuyhdfdhjncxcbjhKJLKJjlkgfjwkfjkjakjlkjd AJDJDJSJDSJDKADJWQKJDKWQJDQIUFUTOIUYUJ'
+
+    order_sn = ''
+    for i in range(20):
+        order_sn += random.choice(s)
+
+    order_sn += str(time.time())
+
+    return order_sn
